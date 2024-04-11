293ea8aa4ebe93e8d3da7724c1e10f6685bba9f2
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-11 19:02:10 -0300

    Adds the Font Awesome library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [
     provideClientHydration()
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faFacebook, faInstagram, faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  faWhatsapp: any;
+  faUser: any;
+  faLinkedin: IconProp = faLinkedin;
+  faTwitter: IconProp = faTwitter;
+  faGithub: IconProp = faGithub;
+  faInstagram: IconProp = faInstagram;
+  faFacebook: IconProp = faFacebook;
+  faEnvelope: IconProp | undefined;
 }
