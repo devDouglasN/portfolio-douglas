59820ae71bb887df80c6a77b0550f84d42d99109
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-12 17:48:11 -0300

    Sobre component created

---
+++ src/app/components/sobre/sobre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faSuitcase, faHeadset, faPaperPlane, faFileLines } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.component.html',
+  styleUrl: './sobre.component.css'
+})
+export class SobreComponent {
+
+}
