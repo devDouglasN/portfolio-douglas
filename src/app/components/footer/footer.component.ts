139e74b77fbe470a3d75ceff8c9ac87ee866a1c0
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-13 14:04:38 -0300

    Fa-icon added for Contato component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faFacebook, faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faGithub = faGithub;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedin;
+  constructor() { }
 
 }
