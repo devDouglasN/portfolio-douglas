38c780299a5b7886d0ecac087fc596e750c4547c
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-17 10:00:33 -0300

    Projeto component created

---
+++ src/app/components/projetos/projetos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-projetos',
+  templateUrl: './projetos.component.html',
+  styleUrl: './projetos.component.css'
+})
+export class ProjetosComponent {
+
+}
