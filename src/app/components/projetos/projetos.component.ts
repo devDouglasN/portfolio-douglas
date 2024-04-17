699eab7ec97453956a1eb18572504ee9c60ea811
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-17 10:27:31 -0300

    Customize the 'Project' component with Swiper Options

--- src/app/components/projetos/projetos.component.ts
+++ src/app/components/projetos/projetos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwiperOptions } from 'swiper/types';
 
 
 @Component({
@@ -8,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class ProjetosComponent {
 
+  config: SwiperOptions = {
+    slidesPerGroup: 5,
+    initialSlide: 0,
+    spaceBetween: 10,
+    navigation: true,
+    centeredSlides: true,
+    keyboard: {
+      enabled: true
+    },
+    loop: true,
+    pagination: {
+      clickable: true
+    },
+    autoplay: true,
+    speed: 1000,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      930: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      1250: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      },
+      1300: {
+        slidesPerView: 5,
+        spaceBetween: 30
+      }
+    }
+  }
 }
