1555b7ec22b579bf391f4da3423f429f71a2244a
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-18 17:54:33 -0300

    Addition of a fa-icon for the 'Formacao' component

--- src/app/components/formacao/formacao.component.ts
+++ src/app/components/formacao/formacao.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { faCalendarDays, faGlobe, faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays, faCirclePlus, faGlobe, faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-formacao',
@@ -13,4 +13,6 @@ export class FormacaoComponent {
   faCalendarDays=faCalendarDays;
   faPenToSquare=faPenToSquare;
  faTrashCan=faTrashCan;
+
+ faCirclePlus=faCirclePlus;
 }
