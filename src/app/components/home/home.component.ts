ddd021839fc9bf1b5eee455f8eb7d45c4af5bc7c
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-25 20:00:15 -0300

    Adds sequential typing animation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,27 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faFacebook, faInstagram, faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
-
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  template: `
-  <div id="typewriter">
-    <p>{{ text }}</p>
-  </div>
-`,
-styles: [`
-  #typewriter {
-    overflow: hidden;
-    white-space: nowrap;
-  }
-`]
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    this.typeEffect.nativeElement.innerHTML = '';
+    this.typeEffect2.nativeElement.innerHTML = '';
+
+    this.typeWriter(this.typeEffect.nativeElement, 'Olá, me chamo Douglas.', 0, 100).then(() => {
+      this.typeWriter(this.typeEffect2.nativeElement, 'Sou desenvolvedor Backend Java', 0, 100);
+    });
+  }
+
+  @ViewChild('typeEffect') typeEffect: ElementRef;
+  @ViewChild('typeEffect2') typeEffect2: ElementRef;
+
+  typeWriter(element, text, i, speed) {
+    return new Promise<void>((resolve, reject) => {
+      const typeWriterInternal = (element, text, i, speed) => {
+        if (i < text.length) {
+          element.innerHTML += text.charAt(i);
+          i++;
+          setTimeout(() => typeWriterInternal(element, text, i, speed), speed);
+        } else {
+          resolve();
+        }
+      }
+      typeWriterInternal(element, text, i, speed);
+    });
+  }
 
   faWhatsapp: any;
   faUser: any;
