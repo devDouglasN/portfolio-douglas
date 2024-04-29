5489815732a5d3797cb3bdeee3361c4307f095d7
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-29 17:13:54 -0300

    Changes to component routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { SobreComponent } from './components/sobre/sobre.component';
-import { FormacaoComponent } from './components/formacao/formacao.component';
-
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'sobre',
-    component: SobreComponent,
-  },
-  {
-    path: 'formacao',
-    component: FormacaoComponent,
-  }
 ];
 
 @NgModule({
