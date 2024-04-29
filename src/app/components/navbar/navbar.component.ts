81e03d336d63f6d537eca93beff70fb6e0968c08
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-29 17:38:51 -0300

    Removal of elements and fa-icons

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { faArrowRightFromBracket, faBars, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +8,6 @@ import { faArrowRightFromBracket, faBars, faUser } from '@fortawesome/free-solid
 })
 export class NavbarComponent {
 
-  faArrowRightFromBracket=faArrowRightFromBracket;
-  faUser=faUser;
-  faBars=faBars;
   closeResult = '';
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void { }
-  login():void{
-    this.router.navigate(['/home-sessao']);
-  }
 }
