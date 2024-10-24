dc65871bb62f9bff67e1f283cf958426302c5bfc
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-10-23 23:44:10 -0300

    Updates

--- src/app/components/certificate/certificate.component.ts
+++ src/app/components/certificate/certificate.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-certificate',
   templateUrl: './certificate.component.html',
-  styleUrls: ['./certificate.component.css'] // Correção aqui
+  styleUrls: ['./certificate.component.css']
 })
 export class CertificateComponent {
 
@@ -11,28 +11,33 @@ export class CertificateComponent {
     {
       title: 'AWS Certified Cloud Practitioner',
       imageUrl: 'https://images.credly.com/size/680x680/images/00634f82-b07f-4bbd-a6bb-53de397fc3a6/image.png',
-      credlyUrl: 'https://www.credly.com/badges/ba3ea7c1-5320-4787-a42a-f4caba45f04c'
+      certificateUrl: 'https://www.credly.com/badges/ba3ea7c1-5320-4787-a42a-f4caba45f04c',
+      hoverText: 'Ver no Credly'
     },
     {
       title: 'Google Cybersecurity Certificate',
       imageUrl: 'https://images.credly.com/size/680x680/images/0bf0f2da-a699-4c82-82e2-56dcf1f2e1c7/image.png',
-      credlyUrl: 'https://www.credly.com/badges/d8b49429-31b2-418c-9569-2aa5bafea1dc/linked_in_profile'
+      certificateUrl: 'https://www.credly.com/badges/d8b49429-31b2-418c-9569-2aa5bafea1dc/linked_in_profile',
+      hoverText: 'Ver no Credly'
     },
     {
       title: 'Introduction to Cybersecurity',
       imageUrl: 'https://images.credly.com/size/680x680/images/af8c6b4e-fc31-47c4-8dcb-eb7a2065dc5b/I2CS__1_.png',
-      credlyUrl: 'https://www.credly.com/badges/3214cd5f-dcfd-4716-a7cf-a07bd6a8e918/linked_in_profile'
+      certificateUrl: 'https://www.credly.com/badges/3214cd5f-dcfd-4716-a7cf-a07bd6a8e918/linked_in_profile',
+      hoverText: 'Ver no Credly'
     },
+    {
+      title: 'Fundamentos em Cibersegurança',
+      imageUrl: 'assets/PT-IC-SEC-1770.png',
+      certificateUrl: 'assets/PT-IC-SEC-1770.png',
+      hoverText: 'Ver Certificado'
+    },
+    {
+      title: 'Linux Essentials',
+      imageUrl: 'assets/linux.png',
+      certificateUrl: 'assets/linux.png',
+      hoverText: 'Ver Certificado'
+    }
   ];
-
-  selectedCertificate: any = null;
-
-  openModal(certificate: any) {
-    this.selectedCertificate = certificate;
-  }
-
-  closeModal() {
-    this.selectedCertificate = null;
-  }
 
 }
